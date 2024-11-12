ee46cc31c3ffdf26132fc6717640845397fb160e
Glitch (aimeehong) <none>
2024-11-12 14:33:57 +0000

    🕌📊 Checkpoint ./main.js:65243141/1911

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
-// fetch("data.json")
-//   .then((r) => r.json())
-//   .then((data) => {
-//     console.log(data);
+fetch("data.json")
+  .then((r) => r.json())
+  .then((data) => {
+    console.log(data);
 
-//     renderNav();
-//     const main = document.querySelector("main");
-//     main.innerHTML = renderProfile(data.about);
-//   });
+    renderNav();
+    const main = document.querySelector("main");
+    main.innerHTML = renderProfile(data.about);
+  });
 
-
-// function renderNav() {
-//   const nav = document.querySelector("nav");
-//   console.log(nav);
-//   nav.innerHTML = `[all the html for nav bar here]`;
-  
-  
-// }
+function renderNav() {
+  const nav = document.querySelector("nav");
+  console.log(nav);
+  nav.innerHTML = `<nav>
+        <ul>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="projects.html">Projects</a></li>
+          <li>
+            <a
+              href="https://cdn.glitch.global/62b052fc-bbde-40a8-a326-98567fcd681a/Hong%20Technical%20Resume.pdf?v=1729235965831"
+              >Resume</a
+            >
+          </li>
+        </ul>
+      </nav>`;
+}
 
 // function renderProfile(about) {
-  
+
 // }
