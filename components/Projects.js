d1ba82d8d4898d7deb74b15a85d1b6bad0330b2b
Glitch (aimeehong) <none>
2024-12-19 20:05:34 +0000

    🐁⛺️ Checkpoint ./style.css:65243141/2 ./data.json:65243141/59 ./components/Projects.js:65243141/598

--- components/Projects.js
+++ components/Projects.js
@@ -3,9 +3,21 @@ export default function renderProjects(data) {
   <section class="projects">
           <h3>Featured Projects</h3>
           <ul class="projectlist">
-            ${data.projects.filter((p) => data.featured.includes(p.id)).map((p) => renderProject(p)).join("")}
+            ${data.projects.filter((p) => data.featured.includes(p.id)).map((p) => renderFeaturedProject(p)).join("")}
           </ul>
         </section>`;
+}
+
+export function renderFeaturedProject(project) {
+  const p = project.materials.find((p) => p.label == "Video");
+  let returned = `
+  <li><a class="projects" href="?project=${project.id}">${project.title}</a></li>
+  <p>${project.description}</p>
+  `;
+  if (p) {
+    returned = returned + `<img src=${p.path} alt="${project.title} Project Image">`
+  }
+  return returned 
 }
 
 export function renderProject(project) {
